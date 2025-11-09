c9bf8482b2343a5d5bfbe1fc518c0f74bee6f910
RawadHaidar <rawadahaidar@gmail.com>
2025-11-09 12:18:55 +0200

    second frontend commit to fix merge

--- frontend/scripts/scripts.js
+++ frontend/scripts/scripts.js
@@ -1,30 +1,50 @@
-// document.createElement
-// document.getElementsByTagName
-// document.createElement
+// function trigger_alert(){
+//     alert("trigger_alert() is connected and working!");
+// }
+
+// input:
 // language
 // filname
 // code
 
+//output:
+// severity
+// file
+// issue
+// suggestion
+
 async function send_review() {
+    
+    //validate input
+    const language = document.getElementById('language').value;
+    const filname = document.getElementById('filename').value;
+    const code = document.getElementById('code').value;
+
+    if(code.trim() === ''){
+        alert("Code field can't be empty.");
+    }
     const payload = {
-        language: document.getElementById('language').value,
-        filename: document.getElementById('filename').value,
-        code: document.getElementById('code').value
+        language: language,
+        filename: filname,
+        code: code
     };
 
     const response = await fetch('review.php', {
         method : 'POST',
-        header: {'Content-Type': 'application/json'},
+        headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(payload)
     });
 
-    const response_data = response.json();
-    //validate response later
+    const response_data = await response.json();
+    //validate response
+
+
+
 
     //build html table to return: th=column, tr=row, td=cell
     let result_html=`<table><tr>Severity</tr><tr>File</tr><tr>Issue</tr><tr>Suggestions</tr>`;
 
-    response_data.array.forEach(element => { //array
+    response_data.forEach(element => { //array
         result_html += `<tr>
             <td class="${element.severity}">${element.severity}</td>
             <td>${element.file}/td>
